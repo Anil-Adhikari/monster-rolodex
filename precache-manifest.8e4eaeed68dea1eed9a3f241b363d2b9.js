e0686a18225e51954b6a533d98e847ae5a3d3f76
anilad <link.anil200@gmail.com>
2020-01-06 12:47:40 -0500

    Updates

---
+++ precache-manifest.8e4eaeed68dea1eed9a3f241b363d2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cbb6ee91e6e3cb8f42c9154a9ba6f5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3e0c0d94be91679b1d89",
+    "url": "/monster-rolodex/static/css/main.7f99f9e5.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e0c0d94be91679b1d89",
+    "url": "/monster-rolodex/static/js/main.414dd0f7.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
